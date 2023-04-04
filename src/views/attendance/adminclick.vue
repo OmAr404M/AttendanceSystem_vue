bb06c6a3ffe5b813edf2f773bb30fd9adea1ef03
OmAr404M <94761500+OmAr404M@users.noreply.github.com>
2023-04-04 03:15:12 +0200

    Update adminclick.vue

--- src/views/attendance/adminclick.vue
+++ src/views/attendance/adminclick.vue
@@ -1,20 +1,14 @@
-{% extends 'school/homebase.html' %}
-
-
-{% block content %}
-
+<template>
 <br>
 <br>
-
 <div class="jumbotron" style="margin-bottom:0px;">
   <h1 class="display-4">Admin Login Panel</h1>
   <p class="lead">Welcome to School Management System.</p>
   <hr class="my-4">
   <p>Please select any to contiue...</p>
   <p class="lead">
-    <a class="btn btn-danger btn-lg" href="adminsignup" role="button">Sign Up</a>
-    <a class="btn btn-success btn-lg" href="adminlogin" role="button">Login</a>
+    <a class="btn btn-danger btn-lg" href="" role="button">Sign Up</a>
+    <a class="btn btn-success btn-lg" href="" role="button">Login</a>
   </p>
 </div>
-
-{% endblock content %}
+</template>
