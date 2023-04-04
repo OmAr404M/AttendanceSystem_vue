8f11d6f91abc541fc6c2fcac819c0b9422f4d516
OmAr404M <94761500+OmAr404M@users.noreply.github.com>
2023-04-04 03:11:38 +0200

    Update index.vue

--- src/views/attendance/index.vue
+++ src/views/attendance/index.vue
@@ -1,73 +1,65 @@
-<!DOCTYPE html>
-{% load static %}
-<html lang="en" dir="ltr">
+<template>
+  <!DOCTYPE html>
+  {% load static %}
+  <html lang="en" dir="ltr">
 
-<head>
-  <meta charset="utf-8">
-  <title></title>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
 
-  <style media="screen">
-    .jumbotron {
-      margin-top: 0px;
-      margin-bottom: 0px;
-      background-image: url('{% static "images/EmpClass.jpg" %}');
-      background-size: cover;
-      background-repeat: no-repeat;
-    }
+  <body> 
+    <br><br>
+    <div class="jumbotron" style="margin-bottom: 0px;margin-top: 0px;">
+      <br>
+      <h1 class="display-4 glow"><b>School Management System</b></h1>
 
-    .jumbotron h1 {
-      text-align: center;
-    }
+      <br><br>
+      <h3 class="text-center" style="color:white;">Built using Python Django</h3>
+      <br><br><br><br><br>
+      <p class="lead">
+        <a class="btn btn-danger btn-lg" href="/" role="button">Sign Up as Student</a>
+      </p>
+      <br><br>
+    </div>
 
-    .alert {
-      margin: 0px;
-    }
-
-
-
-    .glow {
-      font-size: 90px;
-      color: #ffffff;
-      text-align: center;
-      -webkit-animation: glow 1s ease-in-out infinite alternate;
-      -moz-animation: glow 1s ease-in-out infinite alternate;
-      animation: glow 1s ease-in-out infinite alternate;
-    }
-
-    
-  </style>
-
-</head>
-
-<body>
-  {% include "school/navbar.html" %}
-  <br>
-  <br>
-  <div class="jumbotron" style="margin-bottom: 0px;margin-top: 0px;">
     <br>
-    <h1 class="display-4 glow"><b>School Management System</b></h1>
+    <br>
+    <br><br>
 
     <br><br>
-    <h3 class="text-center" style="color:white;">Built using Python Django</h3>
-    <br><br><br><br><br>
-    <p class="lead">
-      <a class="btn btn-danger btn-lg" href="/studentsignup" role="button">Sign Up as Student</a>
-    </p>
-    <br><br>
-  </div>
+    <br>
+    <br>
+    <br>
+    <br>
+  </body>
 
-  <br>
-  <br>
-  <br><br>
+  </html>
+</template>
 
-  {% include "school/admin_teacher_student_card.html" %}
+<style media="screen">
+  .jumbotron {
+    margin-top: 0px;
+    margin-bottom: 0px;
+    background-image: url('');
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
 
-  <br><br>
-  <br>
-  <br>
-  <br>
-  <br>
-  {% include "school/footer.html" %}
-</body>
+  .jumbotron h1 {
+    text-align: center;
+  }
 
-</html>
+  .alert {
+    margin: 0px;
+  }
+
+  .glow {
+    font-size: 90px;
+    color: #ffffff;
+    text-align: center;
+    -webkit-animation: glow 1s ease-in-out infinite alternate;
+    -moz-animation: glow 1s ease-in-out infinite alternate;
+    animation: glow 1s ease-in-out infinite alternate;
+  }
+</style>
